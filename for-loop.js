2deb3ad21c57277e163a2cd574ad235b4553cd4a
rakibhasan47 <mdrakib6585@gmail.com>
2023-01-28 09:12:27 +0600

    git update now

---
+++ for-loop.js
@@ -0,0 +1,19 @@
+// while loop
+var roastGiven = 1;
+while(roastGiven < 7){
+    // console.log('Hi iam rakib hasan');
+    roastGiven++;
+} 
+
+// for loop
+for (var roastGiven = 0; roastGiven < 7; roastGiven++){
+    // console.log('Hello world');
+    // console.log(roastGiven);
+}
+
+for (var i = 0; i < 7; i++){
+    console.log(i);
+}
+
+
+
--- while.loop.js
+++ while.loop.js
@@ -9,5 +9,3 @@ while (roastGiven < 7){
 
 
 
-
-
